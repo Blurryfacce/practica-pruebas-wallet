51d57a9d8c5258f1b1ecabdc8b1309a4b4e27653
Isaac <isaacmh15@gmail.com>
2026-04-24 16:50:38 -0500

    Estructura inicial de SmartWallet y atributos

--- src/main/java/SmartWallet.java
+++ src/main/java/SmartWallet.java
@@ -1,3 +1,19 @@
 public class SmartWallet {
-    
+    private double balance;
+    private boolean isActive;
+    private String userType;
+
+    public SmartWallet() {
+        this.balance = 0.0;
+        this.isActive = true;
+        this.userType = "Standard";
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
 }
