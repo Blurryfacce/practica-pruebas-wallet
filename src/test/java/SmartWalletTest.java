ebbb9254e70db4c9cb2134a0c86d58093711a4be
Isaac <isaacmh15@gmail.com>
2026-04-24 17:07:57 -0500

    Configurando SmartWalletTest y primeros casos de prueba

--- src/test/java/SmartWalletTest.java
+++ src/test/java/SmartWalletTest.java
@@ -1,3 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SmartWalletTest {
-    
+    private SmartWallet wallet;
+
+    @BeforeEach
+    void setUp() {
+        wallet = new SmartWallet();
+    }
+
+    @Test
+    void testDepositStandard() {
+        assertTrue(wallet.deposit(50));
+        assertEquals(50.0, wallet.getBalance());
+    }
+
+    @Test
+    void testWithdrawValid() {
+        wallet.deposit(100);
+        assertTrue(wallet.withdraw(50));
+        assertEquals(50.0, wallet.getBalance());
+    }
 }
